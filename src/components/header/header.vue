3ebf8c108e0da94abb65f7e6f000ae6d8d392dd2
liujun5230 <418873053@qq.com>
2018-03-13 18:29:59 +0800

    sell 项目第一次提交

---
+++ src/components/header/header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="header">
+    这里是头部
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
